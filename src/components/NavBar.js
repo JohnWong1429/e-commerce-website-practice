a6f396a3b312406207fbbedac42104a4a0f6c830
John Wong <cheukpan.wong20@gmail.com>
2023-04-29 13:53:04 +0800

    Add some changes in NavBar.js and css, ScrollToTop.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,29 +9,29 @@ import { CartContext } from '../context/cart_context';
 
 
 const NavBar = () => {
-    const [click, setClick] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
     const { total_items } = useContext(CartContext);
 
     return (
         <div className='navbar'>
             <div className="nav-container">
-                <Link to='/' className='nav-logo'>
+                <Link to='/' className='nav-logo' onClick={() => setShowMenu(false)}>
                     RIW <FaCrown className='crown'/>
                 </Link>
-                <div className="menu-icon" onClick={() => setClick(!click)}>
-                    {click ? <FaTimes /> : <FaBars />}
+                <div className="menu-icon" onClick={() => setShowMenu(!showMenu)}>
+                    {showMenu ? <FaTimes /> : <FaBars />}
                 </div>
-                <ul className={click ? 'navbar-menu active' : 'navbar-menu'}>
+                <ul className={showMenu ? 'navbar-menu active' : 'navbar-menu'}>
                     {LINKS.map(link => (
                         <li key={link.id} className='navbar-item'>
-                            <Link to={link.url} className='nav-links'>
+                            <Link to={link.url} className='nav-links' onClick={() => setShowMenu(false)}>
                                 {link.text}
                             </Link>
                         </li>
                     ))}
                 </ul>
                 <div className="nav-icons">
-                    <Link to='/cart' className='icon'>
+                    <Link to='/cart' className='icon' onClick={() => setShowMenu(false)}>
                         <div className="nav-cartIcon">
                             <AiOutlineShoppingCart />
                             <span>{total_items}</span>
